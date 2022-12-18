791181ed1243f266bedfa63adde165fa29f12bb0
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-12-17 22:43:41 -0500

    adding more coins

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchCoins() {
       const res = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false"
       );
       const data = await res.json();
       setCoins(data);
