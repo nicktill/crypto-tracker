a1c26fa15ad1aa30843e603887cfb7bd82ba187a
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-12-18 02:04:20 -0500

    fixing zoom glitch for safari

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -6,6 +6,10 @@ const Layout = ({ children, title = "crypto-tracker" }) => {
   return (
     <div className={styles.layout}>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        ></meta>
         <title>{title}</title>
         <link rel="shortcut icon" href="/1200x1200.png" />
       </Head>
